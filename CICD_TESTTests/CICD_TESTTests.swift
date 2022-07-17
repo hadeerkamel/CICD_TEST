ef51cf6fb9037bdcbc8fce40b6bafc890fbe3ebd
Hadeer <Hadeer_Kamel@outlook.com>
2022-07-17 22:46:58 +0200

    fix test file

--- CICD_TESTTests/CICD_TESTTests.swift
+++ CICD_TESTTests/CICD_TESTTests.swift
@@ -26,12 +26,7 @@ class CICD_TESTTests: XCTestCase {
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
     
     func testSucess() throws {
         XCTAssertEqual(1, 1)
